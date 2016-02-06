5808f0fc3b3d3e7c172181058145f740db2cf401
ainame <s.namai.09@gmail.com>
2016-02-06 11:57:31 +0900

    Initial Commit

---
+++ mixer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mixer
+//
+//  Created by Namai Satoshi on 2016/02/06.
+//  Copyright © 2016年 ainame. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
