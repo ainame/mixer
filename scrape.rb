4952d540e46461d8f88842649b2fe3e3def892e0
ainame <s.namai.09@gmail.com>
2016-02-08 03:21:42 +0900

    Fix path

--- scrape.rb
+++ scrape.rb
@@ -6,7 +6,7 @@ agent = Mechanize.new
 page = agent.get('http://soundeffect-lab.info/sound/anime/')
 mp3_urls = page.links.select {|link| link.href =~ /download/ }.map{|l| File.join("http://soundeffect-lab.info/", l.href) }
 mp3_urls.each do |url|
-  filename = url.split('/')[-1] + '.mp3'
+  filename = File.join("./mixer/SoundEffects/", url.split('/')[-1] + '.mp3')
   File.binwrite(filename, HTTParty.get(url, follow_redirects: true))
   puts filename
 end
