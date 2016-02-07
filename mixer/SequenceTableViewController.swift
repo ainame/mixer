950c2043e799560452c64e44d8d359dee23bfef3
ainame <s.namai.09@gmail.com>
2016-02-07 16:26:23 +0900

    Add SequenceTableViewController

---
+++ mixer/SequenceTableViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SequenceTableViewController.swift
+//  mixer
+//
+//  Created by Namai Satoshi on 2016/02/07.
+//  Copyright © 2016年 ainame. All rights reserved.
+//
+
+import UIKit
+
+class SequenceTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
