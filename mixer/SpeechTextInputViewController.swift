d936632fc26039c9b39a2deed5a59f522db613a6
ainame <s.namai.09@gmail.com>
2016-02-09 08:51:24 +0900

    Fix ui

--- mixer/SpeechTextInputViewController.swift
+++ mixer/SpeechTextInputViewController.swift
@@ -14,8 +14,7 @@ class SpeechTextInputViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        speechTextView?.becomeFirstResponder()
     }
     
     override func didReceiveMemoryWarning() {
